65a9c5c0096802295732fd06bae7386e4d223576
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-19 20:26:00 +0100

    #28 start with minimap

---
+++ src/MiniMap.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by sam on 19.12.2016.
+ */
+
+import Scatterplot from './Scatterplot';
+
+export default class MiniMap {
+  constructor(private parent: HTMLElement, private plot: Scatterplot<any>) {
+
+  }
+
+  private build() {
+
+  }
+}
