0173a5e09ea865447d3188f7549ad0cc443583df
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-02 16:31:53 +0100

    add typings

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
  */
 
 import './style.scss';
-import Scatterplot from './Scatterplot';
-import DualAxisScatterplot from './DualAxisScatterplot';
+import Scatterplot, {IScatterplotOptions} from './Scatterplot';
+import DualAxisScatterplot, {IScatterplotOptions as DualAxisScatterplotOptions} from './DualAxisScatterplot';
 import * as _symbol from './symbol';
 import * as d3scale from 'd3-scale';
 export {default as Scatterplot, EScaleAxes, IAccessor, IScale, IScatterplotOptions, IWindow, IZoomOptions} from './Scatterplot';
@@ -19,10 +19,10 @@ export const symbol = _symbol;
 
 export default Scatterplot;
 
-export function create<T>(data:T[], canvas:HTMLCanvasElement):Scatterplot<T> {
+export function create<T>(data:T[], canvas:HTMLCanvasElement, options: IScatterplotOptions<T>):Scatterplot<T> {
   return new Scatterplot(data, canvas);
 }
 
-export function dualAxis(data, secondaryData, canvas, options) {
+export function dualAxis<T>(data:T[], secondaryData:T[], canvas:HTMLCanvasElement, options: DualAxisScatterplotOptions<T>) {
   return new DualAxisScatterplot(data, secondaryData, canvas, options);
 }
